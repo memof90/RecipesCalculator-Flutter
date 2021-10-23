55702bba9e23fe4c0f379502372d4cf67c25a507
memo figueredo <memof80@gmail.com>
2021-10-23 16:59:49 -0500

    Putting the list into a card and Making it look nice

--- lib/main.dart
+++ lib/main.dart
@@ -90,11 +90,28 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   // TODO: add buildRecipeCard()
   Widget buildRecipeCard(Recipe recipe) {
-    return Column(
-      children: <Widget> [
-        Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label),
-      ],
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget> [
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(recipe.label, style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino',
+            ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
